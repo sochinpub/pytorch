3e286187c5a4b8916657b20f7b1f4c923387aed4
Jerry Zhang <jerryzh168@gmail.com>
2024-07-16 22:07:04 -0700

    Add `prepare_for_propagation_comparison` and `extract_results_from_loggers`
    Summary:
    Add two top level APIs for numeric debugger in pt2e flow that can log intermediate output in the model
    and calculate summary for metric comparisons between nodes in two graphs
    
    Test Plan:
    python test/test_quantization.py -k test_prepare_for_propagation_comparison
    python test/test_quantization.py -k test_extract_results_from_loggers
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 8ee60c520a75daa133265f05ef63ece944995da4
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/130643

--- torch/ao/quantization/__init__.py
+++ torch/ao/quantization/__init__.py
@@ -18,6 +18,9 @@ from .pt2e.export_utils import _move_exported_model_to_train as move_exported_mo
 from .pt2e.export_utils import _allow_exported_model_train_eval as allow_exported_model_train_eval
 from .pt2e.numeric_debugger import generate_numeric_debug_handle  # noqa: F401
 from .pt2e.numeric_debugger import NUMERIC_DEBUG_HANDLE_KEY  # noqa: F401
+from .pt2e.numeric_debugger import prepare_for_propagation_comparison  # noqa: F401
+from .pt2e.numeric_debugger import extract_results_from_loggers  # noqa: F401
+from .pt2e.numeric_debugger import compare_results  # noqa: F401
 from typing import Union, List, Callable, Tuple, Optional
 from torch import Tensor
 import torch
@@ -147,6 +150,9 @@ __all__ = [
     "weight_observer_range_neg_127_to_127",
     "generate_numeric_debug_handle",
     "NUMERIC_DEBUG_HANDLE_KEY",
+    "prepare_for_propagation_comparison",
+    "extract_results_from_loggers",
+    "compare_results",
 ]
 
 def default_eval_fn(model, calib_data):
